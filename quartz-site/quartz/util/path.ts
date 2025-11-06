b7faf0af80d16e44be36cc0c8b74d89ec4632b7a
brorb <iambrorb@gmail.com>
2025-11-06 19:25:21 +0100

    Fixed 404 error yippiye

--- quartz-site/quartz/util/path.ts
+++ quartz-site/quartz/util/path.ts
@@ -72,7 +72,7 @@ function sluggify(s: string): string {
 export function slugifyFilePath(fp: FilePath, excludeExt?: boolean): FullSlug {
   fp = stripSlashes(fp) as FilePath
   let ext = getFileExtension(fp)
-  const withoutFileExt = fp.replace(new RegExp(ext + "$"), "")
+  const withoutFileExt = ext ? fp.slice(0, -ext.length) : fp
   if (excludeExt || [".md", ".html", undefined].includes(ext)) {
     ext = ""
   }
